fe20a9de6164c583e4b774bca878480bb5fcb62e
Avinash Pandey <24f3001747@ds.study.iitm.ac.in>
2025-09-17 17:03:22 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from sum import add
+from difference import subtract
+from product import multiply
+
+def main():
+    print("Arithmetic Operations:")
+    print("Sum:", add(2, 3))
+    print("Difference:", subtract(10, 4))
+    print("Product:", multiply(6, 7))
+
+if __name__ == "__main__":
+    main()
